dac0b2ec561810fbe26d373ecdbe693fd9ad0355
JashawnRogers <rogersjashawn20@gmail.com>
2025-01-18 12:05:25 -0700

    Added function for current date & changed title

--- scripts.js
+++ scripts.js
@@ -1,46 +1,45 @@
 const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoUL = document.getElementById('todo-list');
+const todaysDate = document.getElementById('date');
 
-let todoList = getTodos();
-console.log(todoList);
+let todoList = getTodo();
 updateTodoList();
+getDate()
 
 todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
     addTodo();
 })
 
-const addTodo = () => {
+function addTodo() {
     const todoText = todoInput.value.trim();
 
     if (todoText.length > 0) {
-        const todoObj = {
+        todoObj = {
             text: todoText,
             completed: false,
         }
-
         todoList.push(todoObj);
-        updateTodoList();
-        saveTodos();
+        updateTodoList()
+        saveTodo();
         todoInput.value = '';
     }
 }
 
 function updateTodoList() {
     todoUL.innerHTML = '';
-
     todoList.forEach((todo, todoIndex) => {
-        todoItem = createNewListItem(todo, todoIndex);
+        todoItem = createNewElement(todo, todoIndex);
         todoUL.append(todoItem);
     })
 }
 
-function createNewListItem(todo,todoIndex) {
-    const todoID = 'todo-'+ todoIndex;
+function createNewElement(todo, todoIndex) {
     const todoLI = document.createElement('li');
+    const todoID = 'todo-'+ todoIndex;
     const todoText = todo.text;
-    todoLI.className = 'todo';
+    todoLI.classList = 'todo';
     todoLI.innerHTML = `
         <input type="checkbox" id="${todoID}">
         <label for="${todoID}" class="custom-checkbox">
@@ -56,34 +55,85 @@ function createNewListItem(todo,todoIndex) {
                 <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
             </svg>
         </button>
+        
     `
-    const deleteBtn = todoLI.querySelector('.delete-btn');
-    deleteBtn.addEventListener('click', () => {
-        deleteTodo(todoIndex);
-    })
-
     const checkbox = todoLI.querySelector('input');
     checkbox.addEventListener('change', () => {
         todoList[todoIndex].completed = checkbox.checked;
-        saveTodos();
+        saveTodo();
     })
 
+    const deleteBtn = todoLI.querySelector('.delete-btn');
+    deleteBtn.addEventListener('click', () => {
+        deleteTodo(todoIndex);
+        
+    })
+    
     checkbox.checked = todo.completed;
     return todoLI;
 }
 
 function deleteTodo(todoIndex) {
     todoList = todoList.filter((_, i) => i !== todoIndex);
-    saveTodos()
+    saveTodo();
     updateTodoList();
 }
 
-function saveTodos() {
-    const todosJSON = JSON.stringify(todoList);
-    localStorage.setItem('todos', todosJSON);
+function saveTodo() {
+    const todoJSON = JSON.stringify(todoList);
+    localStorage.setItem('todos', todoJSON);
 }
 
-function getTodos() {
+function getTodo() {
     const todos = localStorage.getItem('todos') || '[]';
     return JSON.parse(todos);
+}
+
+function getDate() {
+    const date = new Date();
+    let month = date.getMonth();
+    const day = date.getDate();
+    const year = date.getFullYear();
+    console.log(date.getDate());
+    switch (month) {
+        case 0:
+            month = 'January';
+        break;
+        case 1:
+            month ='February';
+        break;
+        case 2:
+            month = 'March';
+        break;
+        case 3:
+            month ='April';
+        break;
+        case 4:
+            month = 'May';
+        case 5:
+            month = 'June'
+        break;
+        case 6:
+            month = 'July'
+        break;
+        case 7:
+            month = 'August';
+        break;
+        case 8:
+            month = 'September';
+        break;
+        case 9:
+            month = 'October';
+        break;
+        case 10:
+            month = 'November';
+        break;
+        case 11:
+            month = 'December';
+        break;
+        default:
+            month = 'ERROR...'
+    }
+
+    todaysDate.innerText = `${month} ${day}, ${year}`;
 }
